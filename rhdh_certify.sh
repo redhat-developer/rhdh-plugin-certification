d662cd3d284dac24fac9fa612232b60c48cc64bf
Dan Curran <dcurran@redhat.com>
2025-03-17 17:06:13 -0500

    adding login command

--- rhdh_certify.sh
+++ rhdh_certify.sh
@@ -1,7 +1,17 @@
 
-echo "DAN TEST"
+timeout --foreground 5m bash <<-"EOF"
+    while ! oc login "$OPENSHIFT_API" -u "$OPENSHIFT_USERNAME" -p "$OPENSHIFT_PASSWORD" --insecure-skip-tls-verify=true; do
+            sleep 20
+    done
+EOF
+if [ $? -ne 0 ]; then
+    echo "Timed out waiting for login"
+    exit 1
+fi
 
+echo "DAN TEST"
 helm list
 
-
+oc login --token="${RHDH_PR_OS_CLUSTER_TOKEN}" --server="${RHDH_PR_OS_CLUSTER_URL}"
+oc whoami --show-server
 
