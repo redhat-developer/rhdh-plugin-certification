4600b9aa2c1674e86673c5ba12a45cc8e6998653
Dan Curran <dcurran@redhat.com>
2025-03-17 23:23:30 -0500

    testing gh output

--- rhdh_certify.sh
+++ rhdh_certify.sh
@@ -10,8 +10,22 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "DAN TEST"
-helm list
 
+for file in ${{ steps.changed-files.outputs.all_changed_files }}; do
+    echo "Processing file: $file"
+    PARTNER_NAME=$(echo "$file" | cut -d'/' -f2)
+    PLUGIN_NAME=$(echo "$file" | cut -d'/' -f3)
+    VERSION=$(echo "$file" | cut -d'/' -f4)
+    echo "PARTNER_NAME=$PARTNER_NAME" >> $GITHUB_OUTPUT
+    echo "PLUGIN_NAME=$PLUGIN_NAME" >> $GITHUB_OUTPUT
+    echo "PACKAGE_YAML=$file" >> $GITHUB_OUTPUT
+    echo "VERSION=$VERSION" >> $GITHUB_OUTPUT
+done
+
+echo $GITHUB_OUTPUT
+
+helm list
+# Force fail by putting this beneath helm list
 oc login --token="${RHDH_PR_OS_CLUSTER_TOKEN}" --server="${RHDH_PR_OS_CLUSTER_URL}"
 oc whoami --show-server
 
