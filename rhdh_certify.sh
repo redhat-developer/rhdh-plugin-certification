fdf1be01c6846c18a9f0017b11b1c5b7d796ff0f
Dan Curran <dcurran@redhat.com>
2025-03-18 16:30:09 -0500

    fixing script to attempt install

--- rhdh_certify.sh
+++ rhdh_certify.sh
@@ -9,26 +9,16 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-echo "DAN TEST"
-
-for file in ${{ steps.changed-files.outputs.all_changed_files }}; do
-    echo "Processing file: $file"
-    PARTNER_NAME=$(echo "$file" | cut -d'/' -f2)
-    PLUGIN_NAME=$(echo "$file" | cut -d'/' -f3)
-    VERSION=$(echo "$file" | cut -d'/' -f4)
-    echo "PARTNER_NAME=$PARTNER_NAME" >> $GITHUB_OUTPUT
-    echo "PLUGIN_NAME=$PLUGIN_NAME" >> $GITHUB_OUTPUT
-    echo "PACKAGE_YAML=$file" >> $GITHUB_OUTPUT
-    echo "VERSION=$VERSION" >> $GITHUB_OUTPUT
-done
-
-echo $GITHUB_OUTPUT
+export IMAGE_LOC=`git log -p --pretty=format: -- certified-plugins.yaml | grep '^[+-]' | grep -Ev '^\+\+\+|^---' | grep image_loc | awk '{print $3}'`
+export IMAGE_VERSION=`git log -p --pretty=format: -- certified-plugins.yaml | grep '^[+-]' | grep -Ev '^\+\+\+|^---' | grep plugin_version | awk '{print $3}'`
 
 
-# Force fail by putting this beneath helm list
-oc login --token="${RHDH_PR_OS_CLUSTER_TOKEN}" --server="${RHDH_PR_OS_CLUSTER_URL}"
-oc whoami --show-server
 
 helm list
+
+helm install \
+    -f rhdh-helm-values.yaml \
+    redhat-developer-hub openshift-helm-charts/redhat-developer-hub \
+    --namespace rose-pipeline --create-namespace
 
 exit 1
