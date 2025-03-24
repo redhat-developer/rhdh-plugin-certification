e108715752c7899af32275d73c1d361a2ab81130
Dan Curran <dcurran@redhat.com>
2025-03-24 13:42:05 -0500

    moving main below function definition

--- .ibm/pipelines/rhdh_certify.sh
+++ .ibm/pipelines/rhdh_certify.sh
@@ -27,6 +27,42 @@ for SCRIPT in "${SCRIPTS[@]}"; do
     echo "Loaded ${SCRIPT}"
 done
 
+handle_main() {
+
+timeout --foreground 5m bash <<-"EOF"
+    while ! oc login "$OPENSHIFT_API" -u "$OPENSHIFT_USERNAME" -p "$OPENSHIFT_PASSWORD" --insecure-skip-tls-verify=true; do
+            sleep 20
+    done
+EOF
+if [ $? -ne 0 ]; then
+    echo "Timed out waiting for login"
+    exit 1
+fi
+
+export IMAGE_LOC=`git log -p --pretty=format: -- certified-plugins.yaml | grep '^[+-]' | grep -Ev '^\+\+\+|^---' | grep image_loc | awk '{print $3}'`
+export IMAGE_VERSION=`git log -p --pretty=format: -- certified-plugins.yaml | grep '^[+-]' | grep -Ev '^\+\+\+|^---' | grep plugin_version | awk '{print $3}'`
+
+helm list
+helm repo add openshift-helm-charts https://charts.openshift.io/
+helm repo update
+
+helm install \
+    -f rhdh-helm-values.yaml \
+    redhat-developer-hub openshift-helm-charts/redhat-developer-hub \
+    --namespace rose-pipeline --create-namespace
+
+
+helm --kubeconfig /opt/.kube/config upgrade --reuse-values -f "${{ needs.detect-changes.outputs.package_yaml }}" \
+    redhat-developer-hub openshift-helm-charts/redhat-developer-hub \
+    --namespace rose-pipeline
+
+helm list
+
+echo "WAITING FOR REVIEW"
+
+sleep 300
+exit 1
+}
 
 main() {
   echo "Log file: ${LOGFILE}"
@@ -61,44 +97,3 @@ exit "${OVERALL_RESULT}"
 }
 
 main
-
-handle_main() {
-
-timeout --foreground 5m bash <<-"EOF"
-    while ! oc login "$OPENSHIFT_API" -u "$OPENSHIFT_USERNAME" -p "$OPENSHIFT_PASSWORD" --insecure-skip-tls-verify=true; do
-            sleep 20
-    done
-EOF
-if [ $? -ne 0 ]; then
-    echo "Timed out waiting for login"
-    exit 1
-fi
-
-export IMAGE_LOC=`git log -p --pretty=format: -- certified-plugins.yaml | grep '^[+-]' | grep -Ev '^\+\+\+|^---' | grep image_loc | awk '{print $3}'`
-export IMAGE_VERSION=`git log -p --pretty=format: -- certified-plugins.yaml | grep '^[+-]' | grep -Ev '^\+\+\+|^---' | grep plugin_version | awk '{print $3}'`
-
-
-
-helm list
-
-
-helm repo add openshift-helm-charts https://charts.openshift.io/
-helm repo update
-
-helm install \
-    -f rhdh-helm-values.yaml \
-    redhat-developer-hub openshift-helm-charts/redhat-developer-hub \
-    --namespace rose-pipeline --create-namespace
-
-
-helm --kubeconfig /opt/.kube/config upgrade --reuse-values -f "${{ needs.detect-changes.outputs.package_yaml }}" \
-    redhat-developer-hub openshift-helm-charts/redhat-developer-hub \
-    --namespace rose-pipeline
-
-helm list
-
-echo "WAITING FOR REVIEW"
-
-sleep 300
-exit 1
-}
