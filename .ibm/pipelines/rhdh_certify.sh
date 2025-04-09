2c0f860ae1ecb3bebe3f522da54951acaed12437
Dan Curran <dcurran@redhat.com>
2025-04-09 14:36:45 -0500

    removing erroneous call in file

--- .ibm/pipelines/rhdh_certify.sh
+++ .ibm/pipelines/rhdh_certify.sh
@@ -22,6 +22,42 @@ SCRIPTS=(
     "utils.sh"
 )
 
+
+run_helm_test_until_success() {
+  local release_name=$1
+  local namespace=$2
+  local helm_test_status=1
+  local start_time=$(date +%s)
+  local timeout=300  # 5 minutes in seconds
+
+  if [ -z "$release_name" ]; then
+    echo "Error: Helm release name is required."
+    return 1
+  fi
+
+  while [ $helm_test_status -ne 0 ]; do
+    echo "Running helm test for release: $release_name"
+    helm test "$release_name" --namespace "$namespace"
+    helm_test_status=$?
+
+    if [ $helm_test_status -eq 0 ]; then
+      echo "Helm test passed for $release_name!"
+      return 0
+    fi
+
+    local current_time=$(date +%s)
+    local elapsed=$((current_time - start_time))
+
+    if [ $elapsed -ge $timeout ]; then
+      echo "Timeout of 5 minutes reached for $release_name. Exiting with status $helm_test_status."
+      return $helm_test_status
+    fi
+
+    echo "Helm test failed with status $helm_test_status. Retrying in 10 seconds..."
+    sleep 10
+  done
+}
+
 # Source each script dynamically
 for SCRIPT in "${SCRIPTS[@]}"; do
     source "${DIR}/${SCRIPT}"
@@ -105,40 +141,6 @@ exit "${OVERALL_RESULT}"
 }
 
 
-run_helm_test_until_success() {
-  local release_name=$1
-  local namespace=$2
-  local helm_test_status=1
-  local start_time=$(date +%s)
-  local timeout=300  # 5 minutes in seconds
-
-  if [ -z "$release_name" ]; then
-    echo "Error: Helm release name is required."
-    return 1
-  fi
-
-  while [ $helm_test_status -ne 0 ]; do
-    echo "Running helm test for release: $release_name"
-    helm test "$release_name" --namespace "$namespace"
-    helm_test_status=$?
-
-    if [ $helm_test_status -eq 0 ]; then
-      echo "Helm test passed for $release_name!"
-      return 0
-    fi
-
-    local current_time=$(date +%s)
-    local elapsed=$((current_time - start_time))
-
-    if [ $elapsed -ge $timeout ]; then
-      echo "Timeout of 5 minutes reached for $release_name. Exiting with status $helm_test_status."
-      return $helm_test_status
-    fi
-
-    echo "Helm test failed with status $helm_test_status. Retrying in 10 seconds..."
-    sleep 10
-  done
-}
 
 
 main
--- .ibm/pipelines/smoke_test.sh
+++ .ibm/pipelines/smoke_test.sh
@@ -1,3 +1,39 @@
+
+test_helm() {
+  local release_name=$1
+  local namespace=$2
+  local helm_test_status=1
+  local start_time=$(date +%s)
+  local timeout=300  # 5 minutes in seconds
+
+  if [ -z "$release_name" ]; then
+    echo "Error: Helm release name is required."
+    return 1
+  fi
+
+  while [ $helm_test_status -ne 0 ]; do
+    echo "Running helm test for release: $release_name"
+    helm test "$release_name" --namespace "$namespace"
+    helm_test_status=$?
+
+    if [ $helm_test_status -eq 0 ]; then
+      echo "Helm test passed for $release_name!"
+      return 0
+    fi
+
+    local current_time=$(date +%s)
+    local elapsed=$((current_time - start_time))
+
+    if [ $elapsed -ge $timeout ]; then
+      echo "Timeout of 5 minutes reached for $release_name. Exiting with status $helm_test_status."
+      return $helm_test_status
+    fi
+
+    echo "Helm test failed with status $helm_test_status. Retrying in 10 seconds..."
+    sleep 10
+  done
+}
+
 smoke_test() {
 
     echo "DAN"
@@ -104,4 +140,5 @@ smoke_test() {
     #       fi    
 }
 
-smoke_test
+# smoke_test
+# test_helm
