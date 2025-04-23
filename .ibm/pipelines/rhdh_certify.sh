5b267edf01864f26de193bbb57ae09c4c8b56bbc
Dan Curran <dcurran@redhat.com>
2025-04-22 19:24:51 -0500

    adding tests to script. next step is reporting results

--- .ibm/pipelines/rhdh_certify.sh
+++ .ibm/pipelines/rhdh_certify.sh
@@ -8,8 +8,20 @@ LOGFILE="test-log"
 export DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 OVERALL_RESULT=0
 
+
+cluster_login() {
+timeout --foreground 5m bash <<-"EOF"
+    while ! oc login "$OPENSHIFT_API" -u "$OPENSHIFT_USERNAME" -p "$OPENSHIFT_PASSWORD" --insecure-skip-tls-verify=true; do
+            sleep 20
+    done
+EOF
+if [ $? -ne 0 ]; then
+    echo "Timed out waiting for login"
+    exit 1
+fi
+}
+
 # Define a cleanup function to be executed upon script exit.
-# shellcheck disable=SC2317
 cleanup() {
   echo "Cleaning up before exiting"
 }
@@ -30,38 +42,25 @@ done
 
 handle_main() {
 
-# handle_main() {
-#   echo "Configuring namespace: ${NAME_SPACE}"
-#   oc_login
-#   echo "OCP version: $(oc version)"
+export NAME_SPACE="test-pipeline"
+export DEPLOYMENT_NAME="redhat-developer-hub"
+export HELM_CHART_VALUE_FILE_NAME_BASE="rhdh-helm-values.yaml"
+export K8S_CLUSTER_ROUTER_BASE=$(oc get route console -n openshift-console -o=jsonpath='{.spec.host}' | sed 's/^[^.]*\.//')
 
-#   export K8S_CLUSTER_ROUTER_BASE=$(oc get route console -n openshift-console -o=jsonpath='{.spec.host}' | sed 's/^[^.]*\.//')
+
+# handle_main() {
 #   cluster_setup
 #   initiate_deployments
 #   deploy_test_backstage_provider "${NAME_SPACE}"
-#   local url="https://${RELEASE_NAME}-backstage-${NAME_SPACE}.${K8S_CLUSTER_ROUTER_BASE}"
-#   check_and_test "${RELEASE_NAME}" "${NAME_SPACE}" "${url}"
-#   local rbac_url="https://${RELEASE_NAME_RBAC}-backstage-${NAME_SPACE_RBAC}.${K8S_CLUSTER_ROUTER_BASE}"
-#   check_and_test "${RELEASE_NAME_RBAC}" "${NAME_SPACE_RBAC}" "${rbac_url}"
+#   local url="https://${DEPLOYMENT_NAME}-backstage-${NAME_SPACE}.${K8S_CLUSTER_ROUTER_BASE}"
+#   check_and_test "${DEPLOYMENT_NAME}" "${NAME_SPACE}" "${url}"
 # }
 
-
-
-timeout --foreground 5m bash <<-"EOF"
-    while ! oc login "$OPENSHIFT_API" -u "$OPENSHIFT_USERNAME" -p "$OPENSHIFT_PASSWORD" --insecure-skip-tls-verify=true; do
-            sleep 20
-    done
-EOF
-if [ $? -ne 0 ]; then
-    echo "Timed out waiting for login"
-    exit 1
-fi
+cluster_login
 
 export IMAGE_LOC=`git log -p --pretty=format: -- certified-plugins.yaml | grep '^[+-]' | grep -Ev '^\+\+\+|^---' | grep image_loc | awk '{print $3}'`
 export IMAGE_VERSION=`git log -p --pretty=format: -- certified-plugins.yaml | grep '^[+-]' | grep -Ev '^\+\+\+|^---' | grep plugin_version | awk '{print $3}'`
 export PLUGIN_FILE=`git show --name-only --oneline HEAD | grep publishers`
-
-git show
 
 # Check if PLUGIN_FILE is empty or contains multiple files
 if [[ -z "$PLUGIN_FILE" ]]; then
@@ -75,31 +74,31 @@ else
     echo "PLUGIN_FILE is valid: $PLUGIN_FILE"
 fi
 
+
+
 helm repo add openshift-helm-charts https://charts.openshift.io/
 helm repo update
 
 echo "Starting helm install"
-# BASE_URL=`oc get dns cluster -o jsonpath='{.spec.baseDomain}'`
-export K8S_CLUSTER_ROUTER_BASE=$(oc get route console -n openshift-console -o=jsonpath='{.spec.host}' | sed 's/^[^.]*\.//')
 helm install \
-    -f rhdh-helm-values.yaml \
+    -f ${HELM_CHART_VALUE_FILE_NAME_BASE} \
     --set global.clusterRouterBase="$K8S_CLUSTER_ROUTER_BASE" \
-    redhat-developer-hub openshift-helm-charts/redhat-developer-hub \
-    --namespace test-pipeline --create-namespace
-echo "Post helm install"
-helm list --namespace test-pipeline
+    ${DEPLOYMENT_NAME} openshift-helm-charts/redhat-developer-hub \
+    --namespace ${NAME_SPACE} --create-namespace
 
-helm_test_until_success redhat-developer-hub test-pipeline
+helm_test_until_success ${DEPLOYMENT_NAME} ${NAME_SPACE}
 
 echo "Starting Upgrade"
 helm upgrade --reuse-values -f "$PLUGIN_FILE" \
-    redhat-developer-hub openshift-helm-charts/redhat-developer-hub \
-    --namespace test-pipeline
-echo "Upgrade Complete"
+    ${DEPLOYMENT_NAME} openshift-helm-charts/redhat-developer-hub \
+    --namespace ${NAME_SPACE}
 
-helm list --namespace test-pipeline
-helm_test_until_success redhat-developer-hub test-pipeline
+helm_test_until_success ${DEPLOYMENT_NAME} ${NAME_SPACE}
 
+local url="https://${DEPLOYMENT_NAME}-${NAME_SPACE}.${K8S_CLUSTER_ROUTER_BASE}"
+
+echo "$url"
+check_upgrade_and_test "${DEPLOYMENT_NAME}" "${RELEASE_NAME}" "${NAME_SPACE}" "${url}"
 smoke_test
 
 echo "WAITING FOR REVIEW"
