590d9a4494da983ed9b8759355352c3e9dffd671
Dan Curran <dcurran@redhat.com>
2025-06-16 13:36:55 -0500

    sleeping to test run

--- .ibm/pipelines/rhdh_certify.sh
+++ .ibm/pipelines/rhdh_certify.sh
@@ -44,20 +44,22 @@ done
 
 handle_main() {
 
-sleep 10000
+echo "DAN1"
 
 export HOME=/tmp
 export DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+echo "DAN2"
 export NAME_SPACE="test-pipeline"
 export DEPLOYMENT_NAME="redhat-developer-hub"
+echo "DAN3"
 export HELM_CHART_VALUE_FILE_NAME_BASE="rhdh-helm-values.yaml"
 export K8S_CLUSTER_ROUTER_BASE=$(oc get route console -n openshift-console -o=jsonpath='{.spec.host}' | sed 's/^[^.]*\.//')
-
+echo "DAN4"
 
 export IMAGE_LOC=`git log -p --pretty=format: -- certified-plugins.yaml | grep '^[+-]' | grep -Ev '^\+\+\+|^---' | grep image_loc | awk '{print $3}'`
 export IMAGE_VERSION=`git log -p --pretty=format: -- certified-plugins.yaml | grep '^[+-]' | grep -Ev '^\+\+\+|^---' | grep plugin_version | awk '{print $3}'`
 export PLUGIN_FILE=`git show --name-only --oneline HEAD | grep publishers`
-
+echo "DAN5"
 # Check if PLUGIN_FILE is empty or contains multiple files
 if [[ -z "$PLUGIN_FILE" ]]; then
     echo "Plugin file $PLUGIN_FILE is empty!"
@@ -71,8 +73,12 @@ else
     echo "PLUGIN_FILE is valid: $PLUGIN_FILE"
 fi
 
+echo "DAN6"
+
 cluster_login
 
+
+echo "DAN7"
 helm repo add openshift-helm-charts https://charts.openshift.io/
 helm repo update
 
@@ -83,6 +89,8 @@ helm install \
     --set global.clusterRouterBase="$K8S_CLUSTER_ROUTER_BASE" \
     ${DEPLOYMENT_NAME} openshift-helm-charts/redhat-developer-hub \
     --namespace ${NAME_SPACE} --create-namespace
+
+echo "DAN8"
 helm_test_until_success ${DEPLOYMENT_NAME} ${NAME_SPACE}
 
 echo "Starting Upgrade"
